3f13f1e4d002f1d7cc9d08af473c9b62ed5167df
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-03 17:01:06 -0500

    added a quantile cutoff approach to dealing with samples with low sequencing

--- 3_seq_depth_error/sampling_depth.R
+++ 3_seq_depth_error/sampling_depth.R
@@ -24,9 +24,9 @@ library(iNEXT)
 #library(broom)
 #library(MASS)
 #library(ggeffects)
-#library(glmmTMB)
-#library(DHARMa)
-#library(MuMIn)
+library(glmmTMB)
+library(DHARMa)
+library(MuMIn)
 #library(effects)
 
 ###########################
@@ -34,14 +34,10 @@ library(iNEXT)
 ###########################
 
 #Dada2 combined runs ASVs, read counts by sample
-comm <- read.csv(here("data", "denoised_data", "dada_may", "combined", "ASVs_counts_all.tsv"), sep = "\t")
-
-#rename columns for simplicity
-colnames(comm) <- sapply(str_split(colnames(comm), "_"), function(x){return(x[[1]])})
-colnames(comm) <- str_remove(colnames(comm), "\\.")
+comm <- read.csv(here("data", "outputs", "2_error_corrected", "all_runs.csv"))
 
 #set row names to ASV labels
-rownames(comm) <- comm$X
+rownames(comm) <- comm$ASV
 
 ###########################
 #Subset data for sequencing depth ####
@@ -49,7 +45,7 @@ rownames(comm) <- comm$X
 
 #select samples minus controls and the ASV column
 depth <- comm %>%
-  dplyr::select(-X, -CL12a, -CL12b, -CL12c, -CL12d, -CL42a, -CL42b, -CL42c,
+  dplyr::select(-X, -ASV, -CL12a, -CL12b, -CL12c, -CL12d, -CL42a, -CL42b, -CL42c,
                 -CL42d, -NEGa, -NEGb, -NEGc, -SMEb, -QC1a, -QC1b, -QC1c,
                 -QC1d)
 
@@ -67,6 +63,9 @@ seq_depth <- iNEXT(depth, q=0, datatype="abundance") #this determines sequencing
 seq_depth$DataInfo$SC
 sample_depth <- seq_depth$DataInfo
 
+quantile(sample_depth$n)
+boxplot.stats(sample_depth$n)$out
+
 #graph the interpolated and extrapolated sampling depth per sample
 ggiNEXT(seq_depth, type=1, facet.var="none", grey = T, se = F) + 
   # can set se = F to remove shaded regions to see lines better 
@@ -76,8 +75,30 @@ ggiNEXT(seq_depth, type=1, facet.var="none", grey = T, se = F) +
         axis.title = element_text(size = 25))
 
 ###########################
+#Quantiles to find change point in data below which sample removal is needed####
+###########################
+quants <- as.data.frame(quantile(sample_depth$n, c(.05, .06, .07, .08, 
+                                                   .09, .10, .11, .12, .13, .14, 
+                                                   .15, .16, .17, .18, .19, .2)))
+
+colnames(quants)
+quants <- quants %>%
+  rename("reads" = "quantile(sample_depth$n, c(0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2))") %>%
+  rownames_to_column(var = "quantile") %>%
+  mutate(number = 1:n()) %>%
+  mutate(number = number +4)
+
+ggplot(quants, aes(x = number, y = reads)) +
+  geom_point() +theme_bw() +
+  geom_hline(yintercept = 11211, linetype = "dashed") +
+  labs(y = "Sequence reads", x = "Quantile")
+
+###########################
 #Sequencing depth analysis by species####
 ###########################
+
+#this was a previous version of exploration, I feel okay about the 
+#quantile cutoff above, so not going to explore this anymore
 
 #I wonder if this would be different if I concatenated the shared taxonomies together
 #First. The few samples which have really low read abundance, according to the above
@@ -96,8 +117,8 @@ taxonomies <- read.csv(here("data", "outputs", "1_taxonomic_assignment",
 
 #summarise target DNA
 target_comm <- comm %>%
-  rename("ASV" = "X") %>%
-  gather(sample, reads, CEN10b:SMEb) %>%
+  dplyr::select(-X) %>%
+  gather(sample, reads, NEGa:QC1d) %>%
   left_join(taxonomies, by = "ASV") %>%
   group_by(sample, unique_ID) %>%
   summarise(reads = sum(reads)) %>%
@@ -129,26 +150,9 @@ ggiNEXT(seq_depth_cat, type=1, facet.var="none", grey = T, se = F) +
   theme(legend.position = "none", axis.text = element_text(size = 20), 
         axis.title = element_text(size = 25))
 
-
-###########################
-#How else to make a cutoff for low sequencing depth... GRRRR ####
-###########################
-#summarise target DNA
-low <- comm %>%
-  rename("ASV" = "X") %>%
-  gather(sample, reads, CEN10b:SMEb) %>%
-  left_join(taxonomies, by = "ASV") %>%
-  group_by(sample, unique_ID) %>%
-  summarise(reads = sum(reads)) %>%
-  filter(!sample %in% c("CL12a", "CL12b", "CL12c", "CL12d", "CL42a", "CL42b", "CL42c",
-                        "CL42d", "NEGa", "NEGb", "NEGc", "QC1a", "QC1b", "QC1c", "QC1d",
-                        "SMEb"))  %>%
-  filter(reads > 0)
-
 ###########################
 #Non target considerations below####
 ###########################
-
 tax_categories <- read.csv(here("data", "outputs", "1_taxonomic_assignment", 
                                 "all_ASV_tax.csv"))
 
