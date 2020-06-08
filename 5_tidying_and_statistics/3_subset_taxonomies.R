c5373c3ac9963c6d4077428f6c3da64b8624b433
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-08 09:48:19 -0500

    started taxonomy subset script to remove predator DNA from prey DNA in each sample

---
+++ 5_tidying_and_statistics/3_subset_taxonomies.R
@@ -0,0 +1,63 @@
+###########################
+#Subset taxonomies
+#Ana Miller-ter Kuile
+#June 8, 2020
+###########################
+
+#Need to subset prey taxonomies per predator from the rarefied dataset. 
+#This will include giving the predators names that match the ASV ID, 
+#so that I can filter out the ASVs where these match from any diet 
+#analysis
+
+###########################
+#Load Packages ####
+###########################
+library(here)
+library(tidyverse)
+
+###########################
+#Load Data ####
+###########################
+#Prey DNA taxonomies
+taxa <- read.csv(here("data", "outputs", "1_taxonomic_assignment", "ASV_taxonomies.csv"))
+
+#load community data
+comm <- read.csv(here("data", "outputs", "4_rarefied", "community_rare.csv"))
+
+comm <- comm %>%
+  rename("ASV" = "X.1") %>%
+  dplyr::select(-X) 
+
+###########################
+#Manipulate Data to Long and Assign Predator IDs ####
+###########################
+
+comm_long <- comm %>%
+  gather(sample, reads, HEV01a:HEV99d)
+
+#Heteropoda venatoria
+#Neoscona theisi
+#Scyotodes Striatipes
+#Centipede - Geophilomorpha, Tygarrup javanicus
+#Phisis holdhausi, Tettigoniidae
+#Smeringopus palidus
+#Euborellia annulipes
+#Pantala flavescens
+#Keijia mneon, Platnickina mneon
+
+#Geophilomorpha
+#Keijia mneon
+#Neoscona theisi
+#Neoscona
+#Pantala flavescens
+#Smeringopus pallidus
+#Sparassidae
+#Tygarrup javanicus
+#Tettigoniidae
+
+
+
+
+preds <- preds %>% 
+  dplyr::mutate(pred_ID=ifelse(str_detect(sample, "PAN"),
+                               "Pantala flavescens","Neoscona theisi"))
