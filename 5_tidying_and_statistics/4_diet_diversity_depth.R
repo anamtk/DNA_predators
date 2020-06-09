060700f94216e6527babe4efe4895f6974297d77
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-09 13:33:19 -0500

    started script looking at diet diversity and depth by species

---
+++ 5_tidying_and_statistics/4_diet_diversity_depth.R
@@ -0,0 +1,11 @@
+###########################
+#Diet Diversity and Depth
+#Ana Miller-ter Kuile
+#June 9, 2020
+###########################
+
+#This code will look at how diet diversity by predator is represented
+#in our dataset
+#Do some predator species have broader diets?
+#Do some predator individuals of different species have more in an individual diet?
+#Did we capture the majority of the diet of our predators?
