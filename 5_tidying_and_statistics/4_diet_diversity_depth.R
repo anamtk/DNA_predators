860f949f7770153f4c7de1cee39a03b7bc82142f
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-10 13:19:58 -0500

    started playing with species accumulation curves from vegan, mostly exploratory right now

--- 5_tidying_and_statistics/4_diet_diversity_depth.R
+++ 5_tidying_and_statistics/4_diet_diversity_depth.R
@@ -22,6 +22,7 @@ library(effects)
 library(emmeans)
 library(DHARMa)
 library(MuMIn)
+library(vegan)
 
 ##########################
 #Load data####
@@ -108,7 +109,7 @@ abund_depth$DataInfo$SC
 abund <- abund_depth$DataInfo
 
 #graph the interpolated and extrapolated sampling depth per sample
-ggiNEXT(abund_depth, type=1, facet.var="none") + 
+ggiNEXT(abund_depth, type=1, facet.var="site") + 
   # can set se = F to remove shaded regions to see lines better 
   theme_bw() +
   labs(x = "Sampling Depth", y = "Species Abundance", title = "Number of Individuals") +
@@ -128,3 +129,260 @@ ggiNEXT(pres_depth, type=1, facet.var="none") +
   theme(axis.text = element_text(size = 20), 
         axis.title = element_text(size = 25))
 
+##########################
+#vegan####
+###########################
+
+sample_counts <- dna2 %>%
+  left_join(meta, by = c("sample" = "Extraction.ID")) %>%
+  distinct(ID, sample, No..Individuals) %>%
+  dplyr::select(ID, sample, No..Individuals)
+  
+
+geo <- dna2 %>%
+  filter(pred_ID == "Geophilomorpha") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+rowSums(geo)
+
+geo1 <- specaccum(geo)
+geo2 <- specaccum(geo, "random")
+
+plot(geo1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(geo2, col="yellow", add=TRUE, pch="+")
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+eub <- dna2 %>%
+  filter(pred_ID == "Euborellia annulipes") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+eub_samps <- eub %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+eub_wts <- eub_samps$No..Individuals
+
+eub1 <- specaccum(eub)
+eub2 <- specaccum(eub, "random", w = eub_wts)
+
+plot(eub1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(eub2, col="yellow", add=TRUE, pch="+")
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+hev <- dna2 %>%
+  filter(pred_ID == "Heteropoda venatoria") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+hev_samps <- hev %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+  
+hev_wts <- hev_samps$No..Individuals
+
+hev1 <- specaccum(hev)
+hev2 <- specaccum(hev, "random", w = hev_wts)
+
+plot(hev1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(hev2, col="yellow", add=TRUE, pch="+")
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+lrs <- dna2 %>%
+  filter(pred_ID == "Keijia mneon") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+lrs_samps <- lrs %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+lrs_wts <- lrs_samps$No..Individuals
+
+lrs1 <- specaccum(lrs)
+lrs2 <- specaccum(lrs, "random", w = lrs_wts)
+
+plot(lrs1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(lrs2, col="yellow", add=TRUE, pch="+")
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+neo <- dna2 %>%
+  filter(pred_ID == "Neoscona theisi") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+neo_samps <- neo %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+neo_wts <- neo_samps$No..Individuals
+
+neo1 <- specaccum(neo)
+neo2 <- specaccum(neo, "random", w = neo_wts)
+
+plot(neo1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(neo2, col="yellow", add=TRUE, pch="+")
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+pan <- dna2 %>%
+  filter(pred_ID == "Pantala flavescens") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+
+pan1 <- specaccum(pan)
+pan2 <- specaccum(pan, "random")
+
+plot(pan1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(pan2, col="yellow", add=TRUE, pch="+")
+
+#########################
+#Phisis holdausi
+########################
+
+phh <- dna2 %>%
+  filter(pred_ID == "Phisis holdhausi") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+phh_samps <- phh %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+phh_wts <- phh_samps$No..Individuals
+
+phh1 <- specaccum(phh)
+phh2 <- specaccum(phh, "random", w=phh_wts)
+
+plot(phh1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(phh2, col="yellow", add=TRUE, pch="+")
+
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+scy <- dna2 %>%
+  filter(pred_ID == "Scytodes longipes") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+scy_samps <- scy %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+scy_wts <- scy_samps$No..Individuals
+
+scy1 <- specaccum(scy)
+scy2 <- specaccum(scy, "random", w=scy_wts)
+
+plot(scy1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(scy2, col="yellow", add=TRUE, pch="+")
+
+
+#"Geophilomorpha"       "Euborellia annulipes" "Heteropoda venatoria"
+##[4] "Keijia mneon"         "Neoscona theisi"      "Pantala flavescens"  
+#[7] "Phisis holdhausi"     "Scytodes longipes"    "Smeringopus pallidus"
+
+sme <- dna2 %>%
+  filter(pred_ID == "Smeringopus pallidus") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+sme_samps <- sme %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+sme_wts <- sme_samps$No..Individuals
+
+sme1 <- specaccum(sme)
+sme2 <- specaccum(sme, "random", w=sme_wts)
+
+plot(sme1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(sme2, col="yellow", add=TRUE, pch="+")
+
+
+####################
+#Streamline####
+###################
+
+#extract predator 
+sme <- dna2 %>%
+  filter(pred_ID == "Smeringopus pallidus") %>%
+  dplyr::select(sample, unique_ID, reads) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  pivot_wider(names_from = "unique_ID",
+              values_from = "reads") %>%
+  column_to_rownames(var = "sample")
+
+sme_samps <- sme %>%
+  rownames_to_column(var = "sample") %>%
+  left_join(sample_counts, by = "sample") %>%
+  dplyr::select(sample, No..Individuals)
+
+sme_wts <- sme_samps$No..Individuals
+
+sme1 <- specaccum(sme)
+sme2 <- specaccum(sme, "random", w=sme_wts)
+
+plot(sme1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
+boxplot(sme2, col="yellow", add=TRUE, pch="+")
