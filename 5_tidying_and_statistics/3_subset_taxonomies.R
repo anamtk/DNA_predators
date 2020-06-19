bd2581252d6fbd7b3b8788d187b2eef377f24b22
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-19 14:10:56 -0500

    updated code with new taxonomic assignments and rarefaction

--- 5_tidying_and_statistics/3_subset_taxonomies.R
+++ 5_tidying_and_statistics/3_subset_taxonomies.R
@@ -20,15 +20,16 @@ library(fuzzyjoin) #fuzzy inner join for string detection join
 #Load Data ####
 ###########################
 #Prey DNA taxonomies
-taxa <- read.csv(here("data", "outputs", "1_taxonomic_assignment", "ASV_taxonomies.csv"))
+taxa <- read.csv(here("data", "outputs", "1_taxonomic_assignment", "ASV_taxonomies_summed_wIndiv.csv"))
+taxa <- taxa %>%
+  dplyr::select(-X)
 
 preds <- read.csv(here("data", "Predator_IDs.csv"))
 #load community data
 comm <- read.csv(here("data", "outputs", "4_rarefied", "community_rare.csv"))
 
 comm <- comm %>%
-  rename("ASV" = "X.1") %>%
-  dplyr::select(-X) 
+  rename("ASV" = "X")
 
 ###########################
 #Manipulate Data to Long and Attach Predator IDs ####
@@ -46,7 +47,8 @@ comm_long <- comm_long %>%
 #Attach prey IDs for each ASV ####
 ###########################
 taxa_comm <- comm_long %>%
-  left_join(taxa, by = "ASV")
+  left_join(taxa, by = "ASV") %>%
+  filter(Domain == "Eukaryota") #remove NA taxonomies from this community
 
 ###########################
 #subset by species to sort ####
@@ -67,21 +69,11 @@ taxa_comm <- comm_long %>%
 HEV <- taxa_comm %>%
   filter(sample_str == "HEV")
 
-#ID matched to species?
-#HEV %>% 
-#  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-#HEV %>% 
- # filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-heva <- HEV %>% 
-  filter(pred_Family == unique_ID)
-
-#ID matched on bold?
-hevb <- HEV %>% 
-  filter(pred_ID == ID_bold)
+#subset predator DNA from this:
+HEV_pred <- HEV %>%
+  ungroup() %>%
+  filter(Order == "Araneae") %>% #only order for spiders
+  filter(Family %in% c("Sparassidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##NEO ####
@@ -90,21 +82,10 @@ hevb <- HEV %>%
 NEO <- taxa_comm %>%
   filter(sample_str == "NEO")
 
-#ID matched to species?
-neoa <- NEO %>% 
-  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-neob <- NEO %>% 
-  filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#NEO %>% 
-#  filter(pred_Family == unique_ID)
-
-#ID matched to bold?
-neoc <- NEO %>% 
-  filter(pred_ID == ID_bold)
+NEO_pred <- NEO %>%
+  ungroup() %>%
+  filter(Order == "Araneae") %>% #only order for spiders
+  filter(Family %in% c("Araneidae", "", "Theridiidae")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##SCY ####
@@ -113,21 +94,10 @@ neoc <- NEO %>%
 SCY <- taxa_comm %>%
   filter(sample_str == "SCY")
 
-#ID matched to species?
-#SCY %>% 
-#  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-#SCY %>% 
-#  filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#SCY %>% 
-#filter(pred_Family == unique_ID)
-
-#ID matched to bold?
-scya <- SCY %>% 
-  filter(pred_ID == ID_bold)
+SCY_pred <- SCY %>%
+  ungroup() %>%
+  filter(Order == "Araneae") %>% #only order for spiders
+  filter(Family %in% c("Scytodidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##CEN ####
@@ -137,13 +107,9 @@ scya <- SCY %>%
 CEN <- taxa_comm %>%
   filter(sample_str == "CEN")
 
-#ID matched to order?
-cena <- CEN %>% 
-  filter(pred_ID == unique_ID)
-
-#ID matched to bold?
-#CEN %>% 
-#  filter(pred_ID == ID_bold)
+CEN_pred <- CEN %>%
+  ungroup() %>%
+  filter(Class == "Chilopoda") #only class of centipedes
 
 ###########################
 ##PHH ####
@@ -153,25 +119,10 @@ cena <- CEN %>%
 PHH <- taxa_comm %>%
   filter(sample_str == "PHH")
 
-#ID matched to species?
-#PHH %>% 
-#  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-##PHH %>% 
- #filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#PHH %>% 
-# filter(pred_Family == unique_ID)
-
-#ID matched to order? - not sure i trust this, want to be able to distinguish pred from non
-phha <- PHH %>%
-  filter(pred_Order == unique_ID)
-
-#ID matched to bold order?
-phhb <- PHH %>% 
-  filter(pred_Order == ID_bold)
+PHH_pred <- PHH %>%
+  ungroup() %>%
+  filter(Order == "Orthoptera") %>% #only order for katydids
+  filter(Family %in% c("Tettigoniidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##SME ####
@@ -181,21 +132,10 @@ phhb <- PHH %>%
 SME <- taxa_comm %>%
   filter(sample_str == "SME")
 
-#ID matched to species?
-smea <- SME %>% 
-  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-#SME %>% 
-# filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#SME %>% 
-# filter(pred_Family == unique_ID)
-
-#ID matched to bold?
-smeb <- SME %>% 
-  filter(pred_ID == ID_bold)
+SME_pred <- SME %>%
+  ungroup() %>%
+  filter(Order == "Araneae") %>% #only order for spiders
+  filter(Family %in% c("Pholcidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##EUB ####
@@ -204,29 +144,10 @@ smeb <- SME %>%
 EUB <- taxa_comm %>%
   filter(sample_str == "EUB")
 
-#ID matched to species?
-#EUB %>% 
-#  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-##EUB %>% 
-# filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#EUB %>% 
-# filter(pred_Family == unique_ID)
-
-#Order?
-#EUB %>%
-#  filter(pred_Order == unique_ID)
-
-#ID matched to bold?
-#EUB %>% 
-#  filter(pred_ID == ID_bold)
-
-#Order matched bold ID?
-euba <- EUB %>% 
-  filter(pred_Order == ID_bold)
+EUB_pred <- EUB %>%
+  ungroup() %>%
+  filter(Order == "Dermaptera") %>% #only order for earwigs
+  filter(Family %in% c("Anisolabididae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##LRS ####
@@ -236,21 +157,10 @@ euba <- EUB %>%
 LRS <- taxa_comm %>%
   filter(sample_str == "LRS")
 
-#ID matched to species?
-lrsa <- LRS %>% 
-  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-##LRS %>% 
- #filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-#LRS %>% 
-# filter(pred_Family == unique_ID)
-
-#ID matched to bold?
-#LRS %>% 
-#  filter(pred_ID == ID_bold)
+LRS_pred <- LRS %>%
+  ungroup() %>%
+  filter(Order == "Araneae") %>% #only order for spiders
+  filter(Family %in% c("Theridiidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 ##PAN ####
@@ -259,46 +169,23 @@ lrsa <- LRS %>%
 PAN <- taxa_comm %>%
   filter(sample_str == "PAN")
 
-#ID matched to species?
-pana <- PAN %>% 
-  filter(pred_ID == unique_ID)
-
-#ID matched to genus?
-#PAN %>% 
-# filter(pred_Genus == unique_ID)
-
-#ID matched to family?
-##PAN %>% 
-#filter(pred_Family == unique_ID)
-
-#Order?
-#PAN %>%
-#  filter(pred_Order == unique_ID)
-
-#ID matched to bold?
-panb <- PAN %>% 
-  filter(pred_ID == ID_bold)
+PAN_pred <- PAN %>%
+  ungroup() %>%
+  filter(Order == "Odonata") %>% #only order for dragonflies
+  filter(Family %in% c("Libellulidae", "")) #either matched to the family or no family (being conservative)
 
 ###########################
 #Bind them all together and only take the unique ones ####
 ###########################
-known_pred <- heva %>%
-  bind_rows(hevb) %>%
-  bind_rows(neoa) %>%
-  bind_rows(neob) %>%
-  bind_rows(neoc) %>%
-  bind_rows(scya) %>%
-  bind_rows(cena) %>%
-  bind_rows(phha) %>%
-  bind_rows(phhb) %>%
-  bind_rows(smea) %>%
-  bind_rows(smeb) %>%
-  bind_rows(euba) %>%
-  bind_rows(lrsa) %>%
-  bind_rows(pana) %>%
-  bind_rows(panb) %>%
-  group_by(ASV, sample, reads, pred_ID, unique_ID) %>%
-  distinct()
+known_pred <- CEN_pred%>%
+  bind_rows(EUB_pred) %>%
+  bind_rows(HEV_pred) %>%
+  bind_rows(LRS_pred) %>%
+  bind_rows(NEO_pred) %>%
+  bind_rows(PAN_pred) %>%
+  bind_rows(PHH_pred) %>%
+  bind_rows(SCY_pred) %>%
+  bind_rows(SME_pred)
 
 ###########################
 #Use abundances to inform unknowns####
@@ -309,6 +196,7 @@ known_pred <- heva %>%
 #ASVs from samples that maybe didn't get close enough taxonomies to subset
 #to be able to subset predator DNA that has high read and a higher-level
 #taxonomic assignment that *could* match predator (or prey) DNA
+
 
 pred_dist <- known_pred %>%
   filter(reads > 0)
@@ -326,123 +214,40 @@ pred_reads <- pred_reads %>%
 
 ggplot(pred_reads, aes(x = number, y = reads)) +
   geom_point() +theme_bw() +
-  geom_hline(yintercept = 6158, linetype = "dashed") +
+  geom_hline(yintercept = 12098, linetype = "dashed") +
+  geom_hline(yintercept = 3554, linetype = "dashed") +
   labs(y = "Sequence reads", x = "Quantile")
 
-#According to this, I'd be pretty confident that anything above 6158 sequence
+#According to this, I'd be pretty confident that anything above 12098 sequence
 #reads AND that matched a higher-order taxonomy of the predator species 
-#in question, that it would likely ALSO be predator DNA. 
+#in question, that it would likely ALSO be predator DNA. OR
+#anything *below 3554 that matched to the predator higher taxonomy is 
+#likely prey.
 
 ###########################
 #Revisit predator-subset DF to remove these high abundances ####
 ###########################
 
-#none in this DF above our cutoff
-HEV_prey <- HEV %>%
-  anti_join(heva, by = c("sample", "ASV")) %>%
-  anti_join(hevb, by = c("sample", "ASV"))
-
-#here, too, nothing above our cutoff
-PAN_prey <- PAN %>%
-  anti_join(pana, by = c("sample", "ASV")) %>%
-  anti_join(panb, by = c("sample", "ASV"))
-
-#some still in here 
-CEN_prey <- CEN %>%
-  anti_join(cena, by = c("sample", "ASV")) %>%
-  filter(Class != "Chilopoda") %>% #some chilopoda were not removed above
-  filter(reads < 6158) #some "arthropod" DNA had 10,000+ reads, probs predator
-
-#save that additional predator DNA for later
-cenb <- CEN %>%
-  anti_join(cena, by = c("sample", "ASV")) %>%
-  filter(Class == "Chilopoda") %>% 
-  filter(reads > 6158) 
-
-#nothing in here
-EUB_prey <- EUB %>%
-  anti_join(euba, by = c("sample", "ASV"))
-
-#still some high reads in here
-LRS_prey <- LRS %>%
-  anti_join(lrsa, by = c("sample", "ASV")) %>%
-  filter(reads < 6158)
-
-#save the additional predaot DNA for later
-lrsb <- LRS %>%
-  anti_join(lrsa, by = c("sample", "ASV")) %>%
-  filter(reads > 6158)
-
-#still some in here too
-NEO_prey <- NEO %>%
-  anti_join(neoa, by = c("sample", "ASV")) %>%
-  anti_join(neob, by = c("sample", "ASV")) %>%
-  anti_join(neoc, by = c("sample", "ASV")) %>%
-  filter(reads < 6158)
-
-#save the additional predator DNA for later
-neod <- NEO %>%
-  anti_join(neoa, by = c("sample", "ASV")) %>%
-  anti_join(neob, by = c("sample", "ASV")) %>%
-  anti_join(neoc, by = c("sample", "ASV")) %>%
-  filter(reads > 6158)
-
-#none in here
-PHH_prey <- PHH %>%
-  anti_join(phha, by = c("sample", "ASV")) %>%
-  anti_join(phhb, by = c("sample", "ASV")) 
-
-#still some high ones in here
-SCY_prey <- SCY %>%
-  anti_join(scya, by = c("sample", "ASV")) %>%
-  filter(reads < 6158)
-
-#save predator DNA for later 
-scyb <- SCY %>%
-  anti_join(scya, by = c("sample", "ASV")) %>%
-  filter(reads > 6158)
-
-#none in here
-SME_prey <- SME %>%
-  anti_join(smea, by = c("sample", "ASV")) %>%
-  anti_join(smeb, by = c("sample", "ASV"))
-
 ###########################
 #Bind them all back together ####
 ###########################
-#predator - add additional 
-predator_DNA <- known_pred %>%
-  bind_rows(cenb) %>%
-  bind_rows(lrsb) %>%
-  bind_rows(neod) %>%
-  bind_rows(scyb)
+other_DNA <- taxa_comm %>%
+  anti_join(known_pred, by = c("sample", "ASV"))
 
-other_DNA <- HEV_prey %>%
-  bind_rows(PAN_prey) %>%
-  bind_rows(CEN_prey) %>%
-  bind_rows(EUB_prey) %>%
-  bind_rows(LRS_prey) %>%
-  bind_rows(NEO_prey) %>%
-  bind_rows(PHH_prey) %>%
-  bind_rows(SCY_prey) %>%
-  bind_rows(SME_prey) #319352
+prey_fam <- other_DNA %>%
+  filter(Family != "")
 
-prey_DNA <- other_DNA %>%
-  filter(unique_ID != "NA") %>% #remove unassigned, total 152861
-  group_by(sample, unique_ID, pred_ID, Phylum, Class, Order, Family, 
-           Genus, Species, ID_level) %>% #select only variables i wnat going forward
-  summarise(reads = sum(reads)) %>% #summarise reads by unique ID
-  rename("Phylum_prey" = "Phylum",
-         "Class_prey" = "Class",
-         "Order_prey" = "Order",
-         "Family_prey" = "Family",
-         "Genus_prey" = "Genus",
-         "Species_prey" = "Species") #rename taxonomy columns so they clearly correspond to prey (not predator)
+prey_ord <- other_DNA %>%
+  filter(Order != "")
 
-write.csv(predator_DNA, here("data", "outputs", "5_rarefied_taxonomic_sort", "predator_DNA.csv"))
+write.csv(known_pred, here("data", "outputs", "5_rarefied_taxonomic_sort", "predator_DNA.csv"))
 
-write.csv(prey_DNA, here("data", "outputs", "5_rarefied_taxonomic_sort", "prey_DNA.csv"))
+write.csv(other_DNA, here("data", "outputs", "5_rarefied_taxonomic_sort", "all_prey_DNA.csv"))
 
-write.csv(other_DNA, here("data", "outputs", "5_rarefied_taxonomic_sort", "non-predator_DNA.csv"))
+write.csv(prey_fam, here("data", "outputs", "5_rarefied_taxonomic_sort", "fam_prey_DNA.csv"))
+
+write.csv(prey_ord, here("data", "outputs", "5_rarefied_taxonomic_sort", "ord_prey_DNA.csv"))
+
+
 
 
