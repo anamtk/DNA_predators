ce5581a792158794a7ccc938fd3ec3c39718ac36
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-19 11:25:05 -0500

    added Austen's error check based on positive controls instead of negative

--- 3_seq_depth_error/error_correcting.R
+++ 3_seq_depth_error/error_correcting.R
@@ -34,7 +34,38 @@ comm <- comm %>%
   rename("ASV" = "X")
 
 #######################
-#Subset runs to remove negative ####
+#Assess control ASVs ####
+#######################
+#look at ASVs that were assigned to positive controls, so we can
+#look at whether the community assigned any non-controls to these ASVs
+positive <- comm %>%
+  dplyr::select(ASV, CL12a, CL12b, CL12c, CL12d, CL42a, CL42b, CL42c, CL42d, QC1a, 
+                QC1b, QC1c, QC1d)  %>%
+  gather(sample, reads, CL12a:QC1d) %>%
+  filter(reads > 0) %>%
+  dplyr::select(ASV) %>%
+  unique() %>%
+  as_vector()
+
+#this makes me think I need to be careful about the SME samples, but 
+#we'll see
+negative <- comm %>%
+  dplyr::select(ASV, NEGa, NEGb, NEGc, SMEb) %>%
+  gather(sample, reads, NEGa:SMEb) %>%
+  filter(reads > 0)
+
+#######################
+#Subset controls ASVs from community####
+#######################
+#looks like positive control ASVs were not 
+#assigned to any samples. Austen says this
+#is sufficient correction check?
+
+pos <- comm %>%
+  filter(ASV %in% c(positive))
+
+#######################
+#OLD ANALYSIS: Subset runs to remove negative ####
 #######################
 colnames(comm)
 
