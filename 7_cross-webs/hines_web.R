3c964ced3a586c2659a748d9da1f842b70851531
Ana <ana.miller.ter.kuile@gmail.com>
2020-07-14 09:24:59 -0500

    tidyed hines dataset and output all predation links and per predator predation links

--- 7_cross-webs/hines_web.R
+++ 7_cross-webs/hines_web.R
@@ -16,9 +16,58 @@
 #I've exported the hines data from rmangal already in another
 #script, and have used those node data to assign family-level
 #taxonomies to each species in that dataset. 
+
 ###########################
 # Load packages
 library(here)
 library(tidyverse)
 library(ggplot2)
 ###########################
+
+
+###########################
+# Load data
+###########################
+
+interactions <- read.csv(here("Published_webs", "final_cut",
+                              "hines_2019", "interactions_hines.csv"))
+
+interactions <- interactions %>%
+  dplyr::select(node_from, node_to, type, method)
+
+nodes <- read.csv(here("Published_webs", "final_cut",
+                       "hines_2019", "nodes_hines.csv"))
+
+nodes <- nodes %>%
+  dplyr::select(node_id, original_name, taxon_Kingdom,
+                taxon_Class, taxon_Order, taxon_Family)
+
+predation <- interactions %>%
+  filter(type == "predation") %>%
+  left_join(nodes, by =c("node_to" = "node_id"))
+
+per_pred <- predation %>%
+  group_by(node_from, taxon_Family) %>%
+  tally(name = "links") %>%
+  group_by(node_from) %>%
+  summarise(links = n())
+
+per_pred %>%
+  summarise(min = min(links),
+            max = max(links),
+            mean = mean(links), 
+            total = n(), 
+            sd = sd(links),
+            se = sd/sqrt(total))
+
+write.csv(predation, here("data", "outputs", 
+                          "6_pub_webs", "hines_predation.csv"))
+
+write.csv(per_pred, here("data", "outputs", 
+                          "6_pub_webs", "hines_per_pred.csv"))
+
+
+
+
+
+
