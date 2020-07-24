1cf291384ba845d592059a7b1c81ce9a239ae442
Ana <ana.miller.ter.kuile@gmail.com>
2020-07-24 15:27:09 -0500

    updated stats with family richness correcter instead of species richness correcter

--- 6_statistics/7_links_published_webs.R
+++ 6_statistics/7_links_published_webs.R
@@ -46,6 +46,7 @@ pal <- pal %>%
   tally(name = "links") %>%
   mutate(web = "Palmyra",
          species_richness = 409,
+         family_richness = 69,
          coll_method = "HTS molecular",
          pub_year = 2020,
          web_type = "HTS") %>%
@@ -71,6 +72,18 @@ per_pred$web_sz <- ifelse(per_pred$species_richness <= 8, "zero",
                                         ifelse(per_pred$species_richness > 28 & per_pred$species_richness <= 55, "seventy-five", "one hundred"))))
 
 per_pred$web_sz <- as.factor(per_pred$web_sz)
+
+sizes_fam <- per_pred %>%
+  distinct(family_richness) 
+
+quantile(sizes_fam$family_richness)
+
+per_pred$web_sz_fam <- ifelse(per_pred$family_richness <=1, "zero", 
+                              ifelse(per_pred$family_richness > 0 & per_pred$species_richness <= 4.25, "twenty-five",
+                                     ifelse(per_pred$family_richness > 4.25 & per_pred$family_richness <= 8, "fifty",
+                                            ifelse(per_pred$family_richness > 8 & per_pred$family_richness <= 13.75, "seventy-five", "one hundred"))))
+
+per_pred$web_sz_fam <- as.factor(per_pred$web_sz_fam)
 ###########################
 # visualizations
 ###########################
@@ -89,10 +102,15 @@ ggplot(per_pred, aes(x = pub_year, y = species_richness)) +
   geom_hline(yintercept = 100, linetype = "dashed") +
   theme_bw()
 
+ggplot(per_pred, aes(x = pub_year, y = family_richness)) +
+  geom_point() +
+  geom_hline(yintercept = 20, linetype = "dashed") +
+  theme_bw()
+
 hist(per_pred$links)
 
 ###########################
-# stats: links for HTS vs published methods
+# stats: links for HTS vs published methods with species_richness
 ###########################
 #what is the relationship between web collection method
 #on the number of links per predator species if we take
@@ -117,6 +135,35 @@ testDispersion(fit)
 me <- ggpredict(m1, terms = c("coll_method"))
 plot(me) +
   labs(x = "Link assignment method", y = "Number of links per predator species") +
+  theme(axis.title = element_text(size = 15), axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+###########################
+# stats: links for HTS vs published methods with family_richness
+###########################
+#what is the relationship between web collection method
+#on the number of links per predator species if we take
+#into account that these come from webs of different sizes?
+
+m2 <- glmmTMB(links ~ coll_method + web_sz_fam + (1|web),
+              data = per_pred,
+              family = "genpois")
+
+summary(m2)
+plot(allEffects(m2))
+
+em <- emmeans(m2, "coll_method")
+pairs(em)
+
+em <- emmeans(m2, "web_sz_fam")
+pairs(em)
+
+fit <- simulateResiduals(m2, plot = T)
+testDispersion(fit)
+
+me <- ggpredict(m2, terms = c("coll_method"))
+plot(me) +
+  labs(x = "Link assignment method", y = "Predicted links per predator species") +
   theme(axis.title = element_text(size = 15), axis.text = element_text(size = 10),
         axis.text.x = element_text(angle = 45, hjust = 1))
 
