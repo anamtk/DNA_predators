7de75adbc3c84eb795a38bfb86df59f6db6d0b39
Ana <ana.miller.ter.kuile@gmail.com>
2020-08-18 11:57:17 -0500

    updated and cleaned trophic position assignment cleaning script

--- 5_tidying/6_pubweb_link_families.R
+++ 5_tidying/6_pubweb_link_families.R
@@ -1,11 +1,15 @@
 ###########################
-# Links families
+# Links families Trophic Position Assignment
 # July 22, 2020
 # Ana Miller-ter Kuile
 ###########################
 
-#going to be looking at how many families are in our
-#links across webs. 
+#going to be looking at the trophic positions
+#of diet items across different webs
+#this script imports family data, exports
+#for manual assignment via the literature
+#and then exports the final document with
+#various different trophic levels
 
 ###########################
 # Load packages
@@ -27,7 +31,9 @@ pal <- read.csv(here("data", "outputs",
                      "fam_prey_DNA.csv"))
 
 
-pub <- read.csv(here("data", "outputs", "6_pub_webs", "pub_webs_predation.csv"))
+pub <- read.csv(here("data", "outputs", 
+                     "6_pub_webs", 
+                     "pub_webs_predation.csv"))
 
 ###########################
 # Unique families
@@ -86,13 +92,6 @@ all_fam <- bind_rows(pal, pub) %>%
   dplyr::select(-X) %>%
   unite(tp, trophic_group, predatory_omnivory, sep = "", remove = FALSE)
 
-all_fam %>%
-  distinct(web, species_richness, family_richness) %>%
-ggplot(aes(x = species_richness, y = family_richness)) + 
-  geom_point() +
-  geom_smooth(method = "lm", se =F) +
-  theme_bw()
-
 ###########################
 # Create trophic position variables
 ###########################
@@ -100,48 +99,9 @@ ggplot(aes(x = species_richness, y = family_richness)) +
 all_fam$tp <- factor(all_fam$tp, levels = c("herbivore", "detritovore", "omnivoreno",
                                             "omnivoreyes", "parasite", "parasitoid", "predator"))
 
-all_fam$tl <- ifelse(all_fam$tp == "herbivore", 2,
-                     ifelse(all_fam$tp == "detritovore", 2,
-                            ifelse(all_fam$tp == "omnivoreno", 2,
-                                   ifelse(all_fam$tp == "omnivoreyes", 3,
-                                          ifelse(all_fam$tp == "parasite", 4, 5)))))
-
-all_fam$broad_tl <- ifelse(all_fam$broad_tp == "basal", 2,
-                           ifelse(all_fam$broad_tp == "omnivorous", 3, 
-                                  ifelse(all_fam$broad_tp =="", NA, 4)))
-###########################
-# Quick Vis
-###########################
-
-all_1 <- all_fam %>%
-  group_by(coll_method) %>%
-  summarise(mean_tl = mean(tl, na.rm=T), 
-            total = n(), 
-            sd = sd(tl, na.rm =T), 
-            se = sd/sqrt(total))
-  
-ggplot(all_1, aes(x = coll_method, y = mean_tl)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = mean_tl - se, ymax = mean_tl + se), width = 0.2) +
-  labs(x = "Diet assignment method", y = "Literature Trophic Position") +
-  theme_bw()
-
-ggplot(all_fam, aes(x = coll_method, y = broad_tl)) +
-  geom_boxplot() +
-  theme_bw()
-
-ggplot(all_fam, aes(x = web, y = broad_tl)) +
-  geom_boxplot() +
-  theme_bw()
-
-all_fam %>%
-  group_by(consumer, web, coll_method, species_richness, broad_tp) %>%
-  summarise(proportion = n()/species_richness) %>%
-  filter(broad_tp != "") %>%
-  ggplot(aes(x = coll_method, y = proportion, fill = broad_tp)) +
-  geom_boxplot() +
-  theme_bw() +
-  scale_y_log10()
+all_fam$broad_tp <- ifelse(all_fam$tp == "herbivore" | all_fam$tp == "detritovore" | all_fam$tp == "omnivoreno", "basal",
+                           ifelse(all_fam$tp == "omnivoreyes", "omnivorous", 
+                           ifelse(is.na(all_fam$tp), NA, "predatory")))
 
 ###########################
 # Export
