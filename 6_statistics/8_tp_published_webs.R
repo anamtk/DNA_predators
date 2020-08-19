3709b5a203428686cc88cc3c20a9d93d5fa256c7
Ana <ana.miller.ter.kuile@gmail.com>
2020-08-19 10:08:15 -0500

    got rid of nmds and started working on prey family richness correction for a PERMANOVA

--- 6_statistics/8_tp_published_webs.R
+++ 6_statistics/8_tp_published_webs.R
@@ -47,69 +47,6 @@ links_tp <- links %>%
            pub_year, tp) %>%
   tally(name = "link_number") 
 
-meta_tp <- links_tp %>%
-  filter(!is.na(tp)) %>%
-  dplyr::select(web_consumer, web, family_richness, coll_method,
-                pub_year) %>%
-  distinct()
-
-tp_matrix <- links_tp %>%
-  ungroup() %>%
-  dplyr::select(web_consumer, tp, link_number) %>%
-  filter(!is.na(tp)) %>%
-  pivot_wider(names_from = "tp",
-              values_from = "link_number") 
-
-tp_matrix[is.na(tp_matrix)] <- 0
-
-rownames(tp_matrix) <- tp_matrix$web_consumer
-tp_matrix <- tp_matrix %>%
-  dplyr::select(-web_consumer)
-
-tp_matrix <- tp_matrix[rowSums(tp_matrix) > 0,] #1291
-
-
-#############################
-#NMDS by specific TP
-#############################
-
-#I’m using the metaMDS function from the vegan package
-nmds1 <- metaMDS(tp_matrix, distance = "bray", k=2)
-#then look at a stress plot. This is to evaluate whether the NMDS is actually a good representation of the structure of your data.
-#I don’t know a ton about evaluating these, but it should be roughly a linear stair step
-stressplot(nmds1)
-
-#making a data frame of the points from your NMDS
-nmds1_df<-data.frame(MDS1=nmds1$points[,1], MDS2=nmds1$points[,2])
-
-
-#combining with metadata for plotting
-nmds1_df_meta<-nmds1_df %>%
-  bind_cols(meta_tp)
-
-#I’m gong to plot my points in different colors based on the “Site” that they came from, so I need to make sure that my “Site” is a factor, not numeric.
-nmds1_df_meta$web<-as.factor(nmds1_df_meta$web)
-nmds1_df_meta$coll_method <- as.factor(nmds1_df_meta$coll_method)
-
-nmds1_df_meta_1 <- nmds1_df_meta %>%
-  filter(MDS1 > -34)
-#using ggplot to plot it
-#only points
-ggplot(nmds1_df_meta_1, aes(x=MDS1,y=MDS2, color=web))+
-  geom_point(mapping = aes(x=MDS1, y=MDS2),
-             size = 6)+
-  coord_fixed()+
-  theme_bw()
-
-#ellipse
-ggplot(nmds1_df_meta_1, aes(x=MDS1,y=MDS2, color=coll_method))+
-  geom_point(mapping = aes(x=MDS1, y=MDS2),
-             size = 6)+
-  stat_ellipse(level = 0.90)+
-  coord_fixed()+
-  theme_bw()
-
-
 #############################
 #Summarize by broad TP
 #############################
@@ -120,65 +57,9 @@ links_btp <- links %>%
            pub_year, broad_tp) %>%
   tally(name = "link_number") 
 
-meta_btp <- links_btp %>%
-  filter(broad_tp != "") %>%
-  dplyr::select(web_consumer, web, family_richness, coll_method,
-                pub_year) %>%
-  distinct()
-
-btp_matrix <- links_btp %>%
-  ungroup() %>%
-  dplyr::select(web_consumer, broad_tp, link_number) %>%
-  filter(broad_tp != "") %>%
-  pivot_wider(names_from = "broad_tp",
-              values_from = "link_number") 
-
-btp_matrix[is.na(btp_matrix)] <- 0
-
-rownames(btp_matrix) <- btp_matrix$web_consumer
-btp_matrix <- btp_matrix %>%
-  dplyr::select(-web_consumer)
-
-btp_matrix <- btp_matrix[rowSums(btp_matrix) > 0,] #1291
-
-#############################
-#NMDS by broad TP
-#############################
-
-#I’m using the metaMDS function from the vegan package
-nmds1 <- metaMDS(btp_matrix, distance = "bray", k=2)
-#then look at a stress plot. This is to evaluate whether the NMDS is actually a good representation of the structure of your data.
-#I don’t know a ton about evaluating these, but it should be roughly a linear stair step
-stressplot(nmds1)
-
-#making a data frame of the points from your NMDS
-nmds1_df<-data.frame(MDS1=nmds1$points[,1], MDS2=nmds1$points[,2])
-
-
-#combining with metadata for plotting
-nmds1_df_meta<-nmds1_df %>%
-  bind_cols(meta_btp)
-
-#I’m gong to plot my points in different colors based on the “Site” that they came from, so I need to make sure that my “Site” is a factor, not numeric.
-nmds1_df_meta$web<-as.factor(nmds1_df_meta$web)
-nmds1_df_meta$coll_method <- as.factor(nmds1_df_meta$coll_method)
-
-#using ggplot to plot it
-#only points
-ggplot(nmds1_df_meta_1, aes(x=MDS1,y=MDS2, color=web))+
-  geom_point(mapping = aes(x=MDS1, y=MDS2),
-             size = 6)+
-  coord_fixed()+
-  theme_bw()
-
-#ellipse
-ggplot(nmds1_df_meta_1, aes(x=MDS1,y=MDS2, color=coll_method))+
-  geom_point(mapping = aes(x=MDS1, y=MDS2),
-             size = 2)+
-  stat_ellipse(level = 0.90)+
-  coord_fixed()+
-  theme_bw()
-
+###########################
+# other vis
+###########################
 links_btp <- links_btp %>%
   filter(broad_tp != "")
 
@@ -190,8 +71,19 @@ links_sp <- links %>%
   ungroup() %>%
   dplyr::select(web_consumer, total_link_number)
 
+web_species_tp <- links %>%
+  group_by(web, broad_tp) %>%
+  distinct(resource) %>%
+  summarise(total = n()) %>%
+  filter(broad_tp != "") %>%
+  pivot_wider(names_from = "broad_tp",
+              values_from = "total")
+
+web_species_tp[is.na(web_species_tp)] <- 0
+
 links_btp <- links_btp %>%
-  left_join(links_sp, by = "web_consumer")
+  left_join(links_sp, by = "web_consumer") %>%
+  left_join(web_species_tp, by = "web")
 
 ggplot(links_btp, aes(x = coll_method, y = link_number/total_link_number, fill = broad_tp)) +
   geom_boxplot() + theme_bw()
