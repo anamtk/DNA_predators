601b4c9a70ed59368ab47e9fa4c88917993ba4b4
Ana <ana.miller.ter.kuile@gmail.com>
2020-10-07 13:57:51 -0500

    script and CSVs updated for master list of predator and prey mass length data

--- 5_tidying/7_master_body_size_list.R
+++ 5_tidying/7_master_body_size_list.R
@@ -36,7 +36,7 @@ pal_stages <- read.csv(here("data", "size_data", "pal_stages_mass_length.csv"))
 
 #literature
 pantala_lit <- read.csv(here("data", "size_data", "su_pantala_mass_length.csv"))
-sohlstroem <- read.csv(here("data", "size_data", "Sohlstroem_mass_length.csv"))
+sohlstrom <- read.csv(here("data", "size_data", "Sohlstroem_mass_length.csv"))
 
 #############################
 # Compile size data --------
@@ -44,12 +44,14 @@ sohlstroem <- read.csv(here("data", "size_data", "Sohlstroem_mass_length.csv"))
 #compiled dataframe needs:
 #Class, Order, Family, Genus, Species, Length_mm, Mass_mg, Source
 
+#subset and organize undergrad data
 ana_ug <- ana_ug %>%
   dplyr::select(Order, Family, Genus, Species, Length_mm, Weight_mg) %>%
   rename("Mass_mg" = "Weight_mg") %>%
   mutate(Source = "Ana_UG",
          Class = ifelse(Order == "Orthoptera", "Insecta", "Arachnida"))
  
+#subset and organize node-level data from palmyra
 pal_nodes <- pal_nodes %>%
   dplyr::select(Class, Order.1, Family, Genus, specific_epithet, 
                 Body_Length_Mean_mm, Body_Mass_Mean_mg) %>%
@@ -60,9 +62,10 @@ pal_nodes <- pal_nodes %>%
   filter(!is.na(Mass_mg)) %>%
   mutate(Source = "Pal_nodes")
 
+#subset and organize individual level data from palmyra
 #for this i prioritized individuals from predators since
 #others are averaged for the nodes list above
-pal_stages %>%
+pal_stages <- pal_stages %>%
   dplyr::select(Class, Family, Order, Species_Name, 
                 Length_mm, Mass_mg) %>%
   rename("Species" = "Species_Name") %>%
@@ -80,7 +83,47 @@ pal_stages %>%
                              "Opopaea_deserticola",
                              "Phisis_holdhausi")) %>%
   mutate(Genus = "",
-         Source = "Pal_stages")
+         Source = "Pal_stages") %>%
+  mutate(Mass_mg = as.numeric(Mass_mg))
+
+#organize and subset the literature data
+#Class, Order, Family, Genus, Species, Length_mm, Mass_mg, Source
+pantala_lit <- pantala_lit %>%
+  rename("Mass_mg" = "mass_mg",
+         "Length_mm" = "length_mm") %>%
+  mutate(Class = "Insecta")
+
+sohlstrom <- sohlstrom %>%
+  filter(Geographic_Region == "tropical") %>%
+  dplyr::select(Order, Family, Live_mass_mg, length_mm) %>%
+  rename("Mass_mg" = "Live_mass_mg",
+         "Length_mm" = "length_mm") %>%
+  mutate(Class = "",
+         Source = "Sohlstrom et al. 2018") %>%
+  mutate(Order = str_to_title(Order), #make first character uppercase!!
+         Family = str_to_title(Family))
+
+#final compilation of ALL         
+all_bs <- ana_ug %>%
+  bind_rows(pal_nodes) %>%
+  bind_rows(pal_stages) %>%
+  bind_rows(pantala_lit) %>%
+  bind_rows(sohlstrom)
+
+#############################
+# Subset only predators --------
+#############################
+
+bs_pred <- all_bs %>%
+  filter(Genus %in% c("Phisis", "Heteropoda","Opopaea", "Scytodes",
+                        "Neoscona", "Pantala") |
+           Family %in% c("Oonopidae",  "Scytodidae", "Pholcidae", "Libellulidae", "Anisolabididae") |
+  Species %in% c("Neoscona_theisi", "Phisis_holdhausi", "Heteropoda_venatoria") |
+  Order == "Geophilomorpha")
+
+#############################
+# Subset only prey in DNA --------
+#############################
 
 #master prey family list
 pal <- read.csv(here("data", "outputs",
@@ -90,5 +133,17 @@ pal <- read.csv(here("data", "outputs",
 #remove the sequencing run from sample name
 pal$sample <- str_sub(pal$sample, end=-2)
 
-unique(pal$Family)
+prey_ids <- pal %>%
+  distinct(Class, Order, Family)
 
+bs_prey <- all_bs %>%
+  filter(Family %in% prey_ids$Family |
+           Order %in% c("Entomobryomorpha", "Poduromorpha", "Isopoda", "Psocoptera",
+                        "Geophilomorpha", "Thysanoptera", "Psocoptera")) 
+
+#############################
+# Export --------
+#############################
+
+write.csv(bs_pred, here("data", "outputs", "8_prey_sizes", "pred_mass_length.csv"))
+write.csv(bs_prey, here("data", "outputs", "8_prey_sizes", "prey_mass_length.csv"))
