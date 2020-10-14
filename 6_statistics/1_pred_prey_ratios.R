58fef29026650eb7913be6bcdf874dce4d0b4332
Ana <ana.miller.ter.kuile@gmail.com>
2020-10-14 16:02:10 -0500

    playing with modeling predator prey ratios

--- 6_statistics/1_pred_prey_ratios.R
+++ 6_statistics/1_pred_prey_ratios.R
@@ -35,23 +35,81 @@ data <- read.csv(here("data", "outputs", "8_final_dataset",
 
 size <- data %>%
   dplyr::select(-X, -X.1, -X.x, -ASV, -ID_level, -X.y) %>%
-  mutate(pred_mass_mg = exp(pred_log_mass_mg)) 
+  mutate(pred_mass_mg = exp(pred_log_mass_mg),
+         mean_prey_pred_ratio = mean_prey_mass_mg/pred_mass_mg,
+         min_prey_pred_ratio = min_prey_mass_mg/pred_mass_mg) 
 
 #############################
 #Data explorations of expected important variables -------
 #############################
-ggplot(size, aes(x = pred_mass_mg, y = mean_prey_mass_mg, color = Tools)) +
+ggplot(size, aes(x = pred_mass_mg, y = mean_prey_mass_mg, color = sample_str)) +
   geom_abline(slope = 1, linetype = "dashed") +
   geom_point(size = 3) +
   scale_x_log10() +
   scale_y_log10() +
   theme_bw() +
-  facet_wrap(~ sample_str)
+  facet_wrap(~ Tools)
 
-ggplot(size, aes(x = pred_mass_mg, y = min_prey_mass_mg, color = Tools)) +
+ggplot(size, aes(x = pred_mass_mg, y = mean_prey_mass_mg/pred_mass_mg, color = sample_str)) +
   geom_abline(slope = 1, linetype = "dashed") +
   geom_point(size = 3) +
   scale_x_log10() +
   scale_y_log10() +
   theme_bw() +
-  facet_wrap(~ sample_str)
+  facet_wrap(~ Feeding_mode)
+
+ggplot(size, aes(x = pred_mass_mg, y = min_prey_mass_mg, color = sample_str)) +
+  geom_abline(slope = 1, linetype = "dashed") +
+  geom_point(size = 3) +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_bw() +
+  facet_wrap(~ Tools)
+
+ggplot(size, aes(x = pred_mass_mg, y = min_prey_mass_mg, color = sample_str)) +
+  geom_abline(slope = 1, linetype = "dashed") +
+  geom_point(size = 3) +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_bw() +
+  facet_wrap(~ Feeding_mode)
+
+ggplot(size, aes(x = pred_mass_mg, y = min_prey_mass_mg/pred_mass_mg, color = sample_str)) +
+  geom_abline(slope = 1, linetype = "dashed") +
+  geom_point(size = 3) +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_bw() +
+  facet_wrap(~ Feeding_mode)
+
+#############################
+#Basic model of mean -------
+#############################
+
+m1 <- glmmTMB(mean_prey_pred_ratio ~ pred_log_mass_mg*Feeding_mode + (1|sample_str),
+              data = size)
+
+summary(m1)
+
+dredge(m1)
+
+fit <- simulateResiduals(m1, plot = TRUE)
+
+#############################
+#log transformed model -------
+#############################
+
+m2 <- glmmTMB(min_prey_log_mass_mg ~ pred_log_mass_mg*sample_str,
+              data = size)
+summary(m2)
+dredge(m2)
+
+m3 <- glmmTMB(min_prey_log_mass_mg ~ pred_log_mass_mg + sample_str,
+              data = size)
+
+fit <- simulateResiduals(m3, plot = TRUE)
+
+hist(log(size$mean_prey_pred_ratio))
+hist(log(size$min_prey_pred_ratio))
+hist(size$mean_prey_mass_mg)
+hist(size$min_prey_mass_mg)
