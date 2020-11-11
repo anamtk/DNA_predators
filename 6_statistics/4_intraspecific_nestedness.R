d3b52524a7a5acb522f58c3e2506e6f02ffbb971
Ana <ana.miller.ter.kuile@gmail.com>
2020-11-11 09:33:37 -0600

    added figures and species accumulations

--- 6_statistics/4_intraspecific_nestedness.R
+++ 6_statistics/4_intraspecific_nestedness.R
@@ -17,7 +17,7 @@ package.list <- c("here", "tidyverse",
                   "glmmTMB", "emmeans",
                   "MuMIn", "DHARMa",
                   "effects", "ggeffects",
-                  "vegan")
+                  "vegan", "iNEXT")
 
 ## Installing them if they aren't already on the computer
 new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
@@ -31,7 +31,7 @@ for(i in package.list){library(i, character.only = T)}
 data <- read.csv(here("data", "outputs", "8_final_dataset",
                       "pred_prey_sizes_tp_DNAinteractions.csv"))
 
-size <- data %>%
+size3 <- data %>%
   dplyr::select(-X, -X.x, -X.y, -reads) %>%
   mutate(pred_mass_mg = exp(pred_log_mass_mg)) 
 
@@ -45,11 +45,94 @@ meta <- meta %>%
            Microhabitat, Year, Date.Collected,
            Extraction.ID, ID)
 
-size <- size %>%
+size3 <- size3 %>%
   left_join(meta, by = c("sample" = "Extraction.ID"))
 
+
+# Species accumulation curves ---------------------------------------------
+sample_sizes <- size3 %>%
+  filter(sample_str %in% c("HEV", "PHH", "NEO")) %>%
+  distinct(sample, sample_str) %>%
+  group_by(sample_str) %>%
+  summarise(frequency = n()) %>%
+  mutate(Family = "Total")
+  
+frequencies <- size3 %>%
+  filter(sample_str %in% c ("HEV", "PHH", "NEO")) %>%
+  group_by(sample_str, Family) %>%
+  summarise(frequency = n()) 
+
+hev_freq <- frequencies %>%
+  filter(sample_str == "HEV") %>%
+  ungroup() %>%
+  dplyr::select(-sample_str)
+  
+hev_freq <- rbind(data.frame(Family = "total", frequency = 53), hev_freq) 
+
+hev_freq <- hev_freq %>%
+  dplyr::select(-Family) %>%
+  as_vector()
+
+out_hev <- iNEXT(hev_freq, q=0, datatype = "incidence_freq")
+out_hev$AsyEst
+hev_acc <- ggiNEXT(out_hev, type=1, color.var="site") + 
+  theme_bw() + 
+  theme(legend.position="none",
+        axis.text = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  scale_color_manual(values = c("#EEB00C")) +
+  scale_fill_manual(values = c("#EEB00C")) +
+  geom_ribbon(aes(ymin=42-11, ymax=42+11), color = NA, alpha=0.1) +
+  geom_hline(yintercept = 42, linetype = "dashed", size =1) 
+
+neo_freq <- frequencies %>%
+  filter(sample_str == "NEO") %>%
+  ungroup() %>%
+  dplyr::select(-sample_str)
+
+neo_freq <- rbind(data.frame(Family = "total", frequency = 24), neo_freq) 
+
+neo_freq <- neo_freq %>%
+  dplyr::select(-Family) %>%
+  as_vector()
+
+out_neo <- iNEXT(neo_freq, q=0, datatype = "incidence_freq")
+out_neo$AsyEst
+neo_acc <- ggiNEXT(out_neo, type=1, color.var="site") + 
+  theme_bw() + 
+  theme(legend.position="none",
+        axis.text = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  scale_color_manual(values = c("#89742F")) +
+  scale_fill_manual(values = c("#89742F")) +
+  geom_ribbon(aes(ymin=44-11, ymax=44+11), color = NA, alpha=0.1) +
+  geom_hline(yintercept = 44, linetype = "dashed", size =1) 
+
+phh_freq <- frequencies %>%
+  filter(sample_str == "PHH") %>%
+  ungroup() %>%
+  dplyr::select(-sample_str)
+
+phh_freq <- rbind(data.frame(Family = "total", frequency = 42), phh_freq) 
+
+phh_freq <- phh_freq %>%
+  dplyr::select(-Family) %>%
+  as_vector()
+
+out_phh <- iNEXT(phh_freq, q=0, datatype = "incidence_freq")
+out_phh$AsyEst
+phh_acc <- ggiNEXT(out_phh, type=1, color.var="site") + 
+  theme_bw() + 
+  theme(legend.position="none",
+        axis.text = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  scale_color_manual(values = c("#158D8E")) +
+  scale_fill_manual(values = c("#158D8E")) +
+  geom_ribbon(aes(ymin=36-17, ymax=36+17), color = NA, alpha=0.1) +
+  geom_hline(yintercept = 36, linetype = "dashed", size =1) 
 # Matrices by species -----------------------------------------------
-mat_hev <- size %>%
+
+mat_hev <- size3 %>%
   filter(sample_str == "HEV") %>%
   dplyr::select(sample, Family) %>%
   mutate(presence = 1) %>%
@@ -58,7 +141,16 @@ mat_hev <- size %>%
               values_fill = 0) %>%
   column_to_rownames(var = "sample")
 
-mat_phh <- size %>%
+mat_neo <- size3 %>%
+  filter(sample_str == "NEO") %>%
+  dplyr::select(sample, Family) %>%
+  mutate(presence = 1) %>%
+  pivot_wider(names_from = Family,
+              values_from = presence,
+              values_fill = 0) %>%
+  column_to_rownames(var = "sample")
+
+mat_phh <- size3 %>%
   filter(sample_str == "PHH") %>%
   dplyr::select(sample, Family) %>%
   mutate(presence = 1) %>%
@@ -76,13 +168,120 @@ mat_phh <- size %>%
 #as well as the NODF since it seems really common
 
 ## Matrix
-out <- nestedbetajac(mat_phh)
-out
-out2 <- nestednodf(mat_phh)
-out2
-plot(out2)
+out_hev <- nestedbetajac(mat_hev)
+#out_hev
+out_hev2 <- nestednodf(mat_hev)
+#out_hev2
+#plot(out_hev2)
 ## Use oecosimu to assess the non-randomness
 oecosimu(mat_hev, nestednodf, "quasiswap")
+oecosimu(mat_hev, nestedbetajac, "quasiswap")
 
+## Matrix
+out_neo <- nestedbetajac(mat_neo)
+#out_neo
+out_neo2 <- nestednodf(mat_neo)
+#out_neo2
+#plot(out_neo2)
+## Use oecosimu to assess the non-randomness
+oecosimu(mat_neo, nestednodf, "quasiswap")
+oecosimu(mat_neo, nestedbetajac, "quasiswap")
+
+## Matrix
+out_phh <- nestedbetajac(mat_phh)
+#out_phh
+out_phh2 <- nestednodf(mat_phh)
+#out_phh2
+#plot(out_phh2)
+## Use oecosimu to assess the non-randomness
+oecosimu(mat_phh, nestednodf, "quasiswap")
 oecosimu(mat_phh, nestedbetajac, "quasiswap")
+
+# Plot lack of nestedness -------------------------------------------------
+
+hev_matrix <- size3 %>%
+  filter(sample_str == "HEV") %>%
+  mutate(pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  group_by(pred_mass_mg, Family) %>%
+  summarise(total = n()) %>%
+  pivot_wider(names_from = pred_mass_mg,
+              values_from = total,
+              values_fill = 0) %>%
+  mutate(total = rowSums(.[-1])) %>%
+  arrange(total) %>%
+  mutate(Family = factor(Family, levels = Family)) %>%
+  dplyr::select(-total) %>%
+  pivot_longer(!Family,
+               names_to = "pred_mass_mg",
+               values_to = "total") %>%
+  mutate(total = as.factor(total)) %>%
+  mutate(pred_mass_mg = as.numeric(pred_mass_mg),
+         pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  ggplot(aes(x = pred_mass_mg, y = Family, fill = total)) +
+  geom_tile() +
+  theme_bw() +
+  scale_fill_manual(values = c("#FFFFFF", "#EEB00C", "#EEB00C")) +
+  theme(legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  labs(x = "Increasing predator size", y = "Prey family")
+
+neo_matrix <- size3 %>%
+  filter(sample_str == "NEO") %>%
+  mutate(pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  group_by(pred_mass_mg, Family) %>%
+  summarise(total = n()) %>%
+  pivot_wider(names_from = pred_mass_mg,
+              values_from = total,
+              values_fill = 0) %>%
+  mutate(total = rowSums(.[-1])) %>%
+  arrange(total) %>%
+  mutate(Family = factor(Family, levels = Family)) %>%
+  dplyr::select(-total) %>%
+  pivot_longer(!Family,
+               names_to = "pred_mass_mg",
+               values_to = "total") %>%
+  mutate(total = as.factor(total)) %>%
+  mutate(pred_mass_mg = as.numeric(pred_mass_mg),
+         pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  ggplot(aes(x = pred_mass_mg, y = Family, fill = total)) +
+  geom_tile() +
+  theme_bw() +
+  scale_fill_manual(values = c("#FFFFFF", "#89742F", "#89742F")) +
+  theme(legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  labs(x = "Increaseing predator size", y = "Prey family")
+
+phh_matrix <- size3 %>%
+  filter(sample_str == "PHH") %>%
+  mutate(pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  group_by(pred_mass_mg, Family) %>%
+  summarise(total = n()) %>%
+  pivot_wider(names_from = pred_mass_mg,
+              values_from = total,
+              values_fill = 0) %>%
+  mutate(total = rowSums(.[-1])) %>%
+  arrange(total) %>%
+  mutate(Family = factor(Family, levels = Family)) %>%
+  dplyr::select(-total) %>%
+  pivot_longer(!Family,
+               names_to = "pred_mass_mg",
+               values_to = "total") %>%
+  mutate(total = as.factor(total)) %>%
+  mutate(pred_mass_mg = as.numeric(pred_mass_mg),
+         pred_mass_mg = as.factor(pred_mass_mg)) %>%
+  ggplot(aes(x = pred_mass_mg, y = Family, fill = total)) +
+  geom_tile() +
+  theme_bw() +
+  scale_fill_manual(values = c("#FFFFFF", "#158D8E", "#158D8E")) +
+  theme(legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 20),
+        axis.title = element_text(size = 25)) +
+  labs(x = "Increasing predator size", y = "Prey family")
+
+
 
