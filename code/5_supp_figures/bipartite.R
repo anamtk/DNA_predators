5f9a5626528174660762ad7c15e580da7b9daa58
Ana <ana.miller.ter.kuile@gmail.com>
2021-03-12 15:46:27 -0600

    bipartite and body size summary code started and figures

--- code/5_supp_figures/bipartite.R
+++ code/5_supp_figures/bipartite.R
@@ -4,7 +4,7 @@
 # Feb 24, 2021
 ###########################
 
-# make a bipartite vis of prey diet by web building
+# make a bipartite vis of prey diet 
 
 # Load packages -----------------------------------------------------------
 
@@ -22,32 +22,8 @@ for(i in package.list){library(i, character.only = T)}
 
 data <- read.csv(here("data", 
                        "outputs",  
-                       "8_final_dataset", 
+                       "3g_final_dataset", 
                        "pred_prey_sizes_DNAinteractions.csv"))
-
-#select variables to remove
-size <- data %>%
-  dplyr::select(-X, -reads) %>%
-  mutate(pred_mass_mg = 10^(pred_log_mass_mg)) 
-
-#metadata for samples
-meta <- read.csv(here("data", "Sample_metadata.csv"))
-
-#select variables of interest
-meta <- meta %>%
-  dplyr::select(Method, Island, Habitat,
-                Microhabitat, Year, Date.Collected,
-                Extraction.ID, ID) %>%
-  distinct(Method, Island, Habitat,
-           Microhabitat, Year, Date.Collected,
-           Extraction.ID, ID)
-
-#join to the interactions DF
-size <- size %>%
-  left_join(meta, by = c("sample" = "Extraction.ID")) %>%
-  unite(environment, c("Habitat", "Microhabitat"), sep = "_", remove = F) 
-
-# Data summary -------------------------------------------------------
 
 # Matrix for vis -----------------------------------------------
 prey <- data %>%
@@ -63,29 +39,14 @@ prey_IDs <- prey %>%
   dplyr::select(Order, Family) %>%
   distinct(Order, Family)
 
-prey <- prey %>%
-  mutate(predator = "all")
-
 prey_mat <- prey %>%
   pivot_wider(names_from = sample_str,
               values_from = frequency,
               values_fill = 0) %>%
   column_to_rownames(var = "Family") %>%
-  dplyr::select(-Order)
-
-plotweb(prey_mat,
-        method = "cca", empty = TRUE, labsize = 1, ybig = 1,  y.width.low = 0.1, 
-        y.width.high = 0.1, low.spacing = 0.04, high.spacing = NULL,
-        arrow="no",  col.interaction="grey80", col.high = "grey10", 
-        col.low="grey10",  bor.col.interaction =NA, bor.col.high=NA, 
-        bor.col.low=NA, high.lablength = NULL, low.lablength = NULL,
-        sequence=NULL, low.abun=NULL, low.abun.col="green", 
-        bor.low.abun.col ="black", high.abun=NULL, high.abun.col="red", 
-        bor.high.abun.col="black", text.rot=90, text.high.col="black", 
-        text.low.col="black", adj.high=NULL, adj.low=NULL, plot.axes = FALSE,
-        low.y=1, high.y=1.5, add=FALSE, y.lim=NULL, x.lim=NULL, low.plot=TRUE, 
-        high.plot=TRUE, high.xoff = 0, low.xoff = 0, high.lab.dis = NULL, 
-        low.lab.dis = NULL, abuns.type="additional")
+  dplyr::select(-Order) %>%
+  dplyr::select(HEV, PAN, PHH, EUB, SME, CEN,
+                NEO, SCY, LRS)
 
 plotweb(prey_mat,
         method = "normal", empty = TRUE, labsize = 1, ybig = 1,  y.width.low = 0.1, 
@@ -101,4 +62,6 @@ plotweb(prey_mat,
         high.plot=TRUE, high.xoff = 0, low.xoff = 0, high.lab.dis = NULL, 
         low.lab.dis = NULL, abuns.type="additional")
 
-cal_palette("kelp2", n = 12, type = "continuous")
+
+
+
---
+++ code/5_supp_figures/body_size_summaries.R
@@ -0,0 +1,113 @@
+# Load packages -----------------------------------------------------------
+
+package.list <- c("here", "tidyverse", "bipartite", "patchwork")
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+# Import data -------------------------------------------------------------
+
+data <- read.csv(here("data",
+                      "outputs",
+                      "3g_final_dataset",
+                      "pred_prey_sizes_DNAinteractions.csv"))
+
+all <- read.csv(here("data",
+                     "outputs",
+                     "3e_master_body_size_lists",
+                     "all_mass_length.csv"))
+
+all_pal <- all %>%
+  filter(Source %in% c("Ana_UG", "Palmyra"))
+
+prey <- read.csv(here("data",
+                      "outputs",
+                      "3e_master_body_size_lists",
+                      "prey_mass_length.csv"))
+
+# Body Size distributions -------------------------------------------------
+
+predator_sizes <- data %>%
+  mutate(pred_mass_mg = 10^pred_log_mass_mg) %>%
+  distinct(sample, pred_mass_mg) %>%
+  rename(mass = pred_mass_mg) %>%
+  mutate(type = "predator samples") %>%
+  dplyr::select(mass, type)
+  
+interaction_sizes <- data %>%
+  rename(mass = mean_prey_mass_mg) %>%
+  mutate(type = "prey frequency")
+
+prey_sizes <- data %>%
+  distinct(Family, mean_prey_mass_mg) %>%
+  rename(mass = mean_prey_mass_mg) %>%
+  mutate(type = "prey identity")
+
+community_sizes <- all_pal %>%
+  group_by(Family) %>%
+  summarise(mass = mean(Mass_mg, na.rm = T)) %>%
+  mutate(type = "community") %>%
+  filter(Family != "") %>%
+  dplyr::select(-Family)
+
+sizes <- predator_sizes %>%
+  bind_rows(prey_sizes) %>%
+  bind_rows(interaction_sizes) %>%
+  bind_rows(community_sizes)
+
+ggplot(sizes, aes(x = mass, fill = type))  +
+  geom_density(alpha = 0.6, color = "black") +
+  scale_x_log10() +
+  theme_bw() +
+  facet_wrap(~type, nrow = 4)
+
+# Prey Family Size Dists --------------------------------------------------
+prey <- prey %>%
+  filter(Family != "") %>%
+  filter(!is.na(Mass_mg))
+
+prey %>%
+  distinct(Genus)
+
+
+prey_size_graph <- prey %>%
+  filter(Family != "") %>%
+  mutate(Family = as.factor(Family)) %>%
+  mutate(Family = fct_reorder(Family, Mass_mg, .fun='sd')) %>%
+  ggplot(aes(x = Family, y = Mass_mg)) +
+  geom_violin() +
+  scale_y_log10() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+prey_family_count <- prey %>%
+  filter(Family != "") %>%
+  mutate(Family = as.factor(Family)) %>%
+  mutate(Family = fct_reorder(Family, Mass_mg, .fun = 'sd')) %>%
+  distinct(Genus, Family) %>%
+  group_by(Family) %>%
+  tally() %>%
+  ggplot(aes(x = Family, y = n)) +
+    geom_bar(stat = "identity") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+prey_size_graph / prey_family_count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
